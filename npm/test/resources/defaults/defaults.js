36fa6c4a78d93daf2a814880fba8acc7e2d59f20
Kate Stanley <katheris@uk.ibm.com>
2017-07-05 17:58:54 +0100

    feat(config): Add defaults module
    Add defaults module to manage default and config manipulation.
    
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

---
+++ npm/test/resources/defaults/defaults.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//module for storing default configuration values
+
+var getDefaults = function() {
+    return {appName : {desc : 'Name of the application', type : String, default : 'LibertyProject'},
+    buildType : {desc : 'Build system to use', type : String, default : 'maven'}};
+}
+
+module.exports = {
+  getDefaults : getDefaults
+}
