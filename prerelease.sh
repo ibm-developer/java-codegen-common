0b2d98569aa36d10c8d33e54bdab7ad0b98d9f9c
Katherine Stanley <katheris@uk.ibm.com>
2018-01-09 10:51:10 +0000

    fix(build): Fix travis builds to deploy after first version
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- prerelease.sh
+++ prerelease.sh
@@ -1,20 +1,29 @@
+#!/usr/bin/env bash
 git config user.email "travisci@travis.ibm.com"
 git config user.name "Travis CI"
 git config push.default simple
+
+echo "Running smoke test"
+npm run prerelease
+retval=$?
+if [ $retval != 0 ]; then
+  exit $retval
+fi
 echo "Upgrading using standard-version"
 npm run release
-if [ $? != 0 ]; then
-  exit $?
+retval=$?
+if [ $retval != 0 ]; then
+  exit $retval
 fi
-echo "Creating git branch"
+
+echo "Determining next version"
 PKG_VER_NEXT=`node -e "console.log(require('./package.json').version);"`
+
+echo "Creating git branch"
 BRANCH="updateTo${PKG_VER_NEXT}"
 git checkout -b $BRANCH
-
-git status
-git commit -m "Update test coverage and code scan files"
 # this pull request through this branch will be needed to be reviewed as usual
 git remote rm origin
 git remote add origin $GITHUB_URL_SECURED
-git push --follow-tags origin master
+git push --follow-tags origin $BRANCH
 hub pull-request -b master -m "chore: Merging CHANGELOG and package.json changes"
